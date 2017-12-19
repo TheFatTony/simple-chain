9f04e14344a77e3a4411529817448796e490fdb6
TheFatTony <anton.alexeyev@outlook.com>
2017-12-19 15:52:49 +0300

    initial-draft project skeleton added

---
+++ src/test/java/com/chieftain/simple/chain/simplechain/SimpleChainApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chieftain.simple.chain.simplechain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SimpleChainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
